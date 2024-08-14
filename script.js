c2b2468113ff04231a4ddd707ad24948e4db4194
MAICON356 <115078389+MAICON356@users.noreply.github.com>
2024-08-14 07:34:47 -0300

    me

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {}
+    enunciado = "pergunta 1 "
+         alternativa[
+             "altenativa 1" 
+             "alterntiva 2"
+
+    
+       ]
